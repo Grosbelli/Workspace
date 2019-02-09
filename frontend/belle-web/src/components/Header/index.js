3416ce40eb34c45dc2d3e1f882001de23cfcac33
Andrey Grosbelli <agrosbelli@yahoo.com.br>
2019-02-08 23:39:50 -0200

    08.02

--- frontend/belle-web/src/components/Header/index.js
+++ frontend/belle-web/src/components/Header/index.js
@@ -1,31 +1,26 @@
 import React from 'react'
 import './styles.css'
 
+
 const Header = () => (
-    <header class="main-header" id="header"> Belle Biscuit Website
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="navst">
-            <div class="collapse navbar-collapse" id="main-nav">
-                <div class="navbar-nav ml-auto">
-                    <a class="nav-item nav-link active" href="#page-top">Loja <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#about-us">Sobre</a>
-                    <a class="nav-item nav-link" href="#contact">Contato</a>
+    <header className="main-header" id="header">
+    <div id="imagem"><img src="../../.assets/BackgroundBelle2.jpg" alt="" ></img>
+    <div id="logo"></div><img src="../../.assets/LogoCompleto.png" alt=""></img>
+    </div>
+    
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="navst">
+                <div className="navbar-nav ml-auto collapse navbar-collapse" id="main-nav">
+                    <ul className="nav navbar-nav ml-auto" id="list">
+                        <a id="color" href="/" className="nav-link"><i className="fa fa-camera-retro fa-sm"></i></a>
+                        <a id="color" href="/" className="nav-link"><i className="fa fa-twitter fa-lg"></i></a>
+                        <a id="color" href="/" className="nav-link"><i className="fa fa-facebook"></i></a>
+                    </ul>
+                    <a className="nav-item nav-link active" id="color" href="#page-top">Loja <span className="sr-only">(current)</span></a>
+                    <a className="nav-item nav-link" id="color" href="#about-us">Sobre</a>
+                    <a className="nav-item nav-link" id="color" href="#contact">Contato</a>
                 </div>
-            </div>
     </nav>
 
-        <nav class="navbar navbar-dark fixed-top navbar-expand-md" id="nav-social">
-            <div class="container">
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> ☰
-        </button>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav ml-auto">
-                        <a href="#" class="nav-link"><i class="fa fa-camera-retro fa-sm"></i></a>
-                        <a href="#" class="nav-link"><i class="fa fa-twitter fa-lg"></i></a>
-                        <a href="#" class="nav-link"><i class="fa fa-facebook"></i></a>
-                    </ul>
-                </div>
-            </div>
-         </nav>
     </header>
 
             );
